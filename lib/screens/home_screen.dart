b9fd09df8855c81490236a381561d2f6694bee77
s1500729 <dicenull.dev@gmail.com>
2021-03-07 23:41:11 +0900

    サーバをカードリストとして表示するように変更

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,29 +3,36 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mcstatus_flutter/atoms/named_counter.dart';
 import 'package:mcstatus_flutter/models/server_provider.dart';
+import 'package:mcstatus_flutter/organisms/server_card.dart';
+import 'package:mcstatus_flutter/organisms/server_list_body.dart';
 
 class HomeScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: useProvider(serverProvider.state).when(
-        success: (data) => Center(
-          child: Column(
-            children: data.servers
-                .map((state) => NamedCounter(
+    return SafeArea(
+      child: Scaffold(
+        body: useProvider(serverProvider.state).when(
+          success: (data) => SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: ServerListBody(
+              cardList: data.servers.map(
+                (state) => ServerCard(
+                    namedCounter: NamedCounter(
                       bgColor: Theme.of(context).accentColor,
                       badgeColor:
                           state.online ? Colors.green : Colors.redAccent,
                       text: state.name,
                       count: state.playerCount,
-                    ))
-                .toList(),
+                    ),
+                    text: state.description),
+              ),
+            ),
           ),
+          waiting: () => Center(
+            child: CircularProgressIndicator(),
+          ),
+          failure: (_) => Container(),
         ),
-        waiting: () => Center(
-          child: CircularProgressIndicator(),
-        ),
-        failure: (_) => Container(),
       ),
     );
   }
